5e1d5553f93f4d1ba83f86c9071351185c2c4b07
juanySC <48737486+juanySC@users.noreply.github.com>
2025-03-19 20:06:31 -0600

    añadiendo tarea

---
+++ carpeta_tarea/Tarea1 cuadradoCubo.cpp
@@ -0,0 +1,33 @@
+//Ejercicio 1: Leer un número, y luego generar como salida el cuadrado y el cubo del mismo.
+
+//PARTE 1: LIBRERIAS
+#include <stdio.h>
+#include <math.h>
+
+//PARTE 2: DATOS/VARIABLES
+int numeroCC = 0;
+float numeroCuadrado = 0;
+float numeroCubo = 0;
+
+//PARTE 3: FUNCION PRINCIPAL
+
+int main()
+	//PARTE 4: INICIO DEL CODIDO
+{
+	//PARTE 5: INSTRUCCIONES O CODIGO
+	printf("Este programa calcula el cuadrado y cubo de un número \n");
+	printf("Ingrese el número que desee: \n");
+	scanf("%d",&numeroCC);
+	
+	//formula del cuadraro de un numero
+	numeroCuadrado = numeroCC * numeroCC;
+	
+	//formula del cubo de un numeroCC
+	numeroCubo = numeroCC * numeroCC * numeroCC;
+	
+	printf("El cuadrado del número es: \n");
+	printf("%f \n",numeroCuadrado);
+	printf("El cubo del número es: \n");
+	printf("%f", numeroCubo);
+	//PARTE 6:FIN DEL CODIGO 
+}
---
+++ carpeta_tarea/Tarea1 promedio.cpp
@@ -0,0 +1,33 @@
+//Ejercicio 2: Leer tres números y luego generar como salida el promedio de los mismos
+
+//PARTE 1: LIBRERIAS
+#include <stdio.h>
+#include <math.h>
+
+//PARTE 2: DATOS/VARIABLES
+float numero1 = 0;
+float numero2 = 0;
+float numero3 = 0;
+double promedio = 0;
+
+//PARTE 3: FUNCION PRINCIPAL
+
+int main()
+	//PARTE 4: INICIO DEL CODIDO
+{
+	//PARTE 5: INSTRUCCIONES O CODIGO
+	printf("Este programa calcula el promedio de 3 números \n");
+	printf("Ingrese el primer número: \n");
+	scanf("%f",&numero1);
+	printf("Ingrese el segundo número: \n");
+	scanf("%f", &numero2);
+	printf("Ingrese el tercer número: \n");
+	scanf("%f", &numero3);
+	
+	//formula del promedio
+	promedio = (numero1 + numero2 + numero3)/3 ;
+
+	printf("El promedio total es: \n");
+	printf("%lf \n",promedio);
+	//PARTE 6:FIN DEL CODIGO 
+}
