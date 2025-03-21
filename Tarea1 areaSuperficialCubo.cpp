d5f20b06075c1b4fc8cb712fe94526104709a393
juanySC <48737486+juanySC@users.noreply.github.com>
2025-03-20 21:36:06 -0600

    Add files via upload
    el ejercicio de iva e isr aun tiene error que sale negativo el precio total

---
+++ Tarea1 areaSuperficialCubo.cpp
@@ -0,0 +1,28 @@
+//Ejercicio 4: Solicitar los datos necesarios para mostrar, como salida,
+// el área superficial de un cubo.
+
+//PARTE 1: LIBRERIAS
+#include <stdio.h>
+#include <math.h>
+
+//PARTE 2: DATOS/VARIABLES
+float lado = 0;
+float areaSupCubo = 0;
+
+//PARTE 3: FUNCION PRINCIPAL
+
+int main()
+	//PARTE 4: INICIO DEL CODIDO
+{
+	//PARTE 5: INSTRUCCIONES O CODIGO
+	printf("Este programa calcula el área superficial de un cubo \n");
+	printf("Ingrese el valor de uno de los lados del cubo: \n");
+	scanf("%f",&lado);
+	
+	//formula del area superficial de un cubo
+	areaSupCubo = 6 * lado * lado;
+	
+	printf("\n El área superficial del cubo es: \n");
+	printf("%f \n",areaSupCubo);
+	//PARTE 6:FIN DEL CODIGO 
+}
---
+++ Tarea1 ivaeisr.cpp
@@ -0,0 +1,48 @@
+/*Ejercicio 5: Leer el total de una factura, calcular el IVA (12%) y
+ el ISR (5%), luego calcular y mostrar como salida el total de la
+factura sin impuestos*/
+
+//PARTE 1: LIBRERIAS
+#include <stdio.h>
+#include <math.h>
+
+//PARTE 2: DATOS/VARIABLES
+float montoInicial = 0;
+float montoFinalIva = 0;
+float montoFinalIsr = 0;
+float precioOriginal = 0;
+float iva = 0.12;
+float totalIva = 0;
+float isr = 0.05;
+float totalIsr = 0;
+float operacion = 0;
+
+//PARTE 3: FUNCION PRINCIPAL
+
+int main()
+	//PARTE 4: INICIO DEL CODIDO
+{
+	//PARTE 5: INSTRUCCIONES O CODIGO
+	printf("Este programa calcula el IVA e ISR de una compra \n");
+	printf("Ingrese el valor de la compra: \n");
+	scanf("%f",&montoInicial);
+	
+	//formula del iva
+	totalIva = montoInicial * iva;
+	montoFinalIva = montoInicial - totalIva;
+	
+	//formula del ISR 
+	totalIsr = montoInicial * isr;
+	montoFinalIsr = montoInicial - totalIsr;
+	
+	//Haciendo mi cantidad inicial
+	precioOriginal = montoInicial - montoFinalIva - montoFinalIsr ;
+	
+	printf("\n El precio original sin impuestos es de: Q");
+	printf("%f \n",precioOriginal);
+	printf("\nPor el IVA se suma: Q ");
+	printf("%f",totalIva);
+	printf("\nPor el ISR se suma: Q ");
+	printf("%f",totalIsr);
+	//PARTE 6:FIN DEL CODIGO 
+}
