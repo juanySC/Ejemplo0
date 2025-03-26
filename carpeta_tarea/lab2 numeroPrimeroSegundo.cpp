2846596fa2a3df0db63d212a6ccb0210748960c4
juanySC <48737486+juanySC@users.noreply.github.com>
2025-03-26 09:52:08 -0600

    Primeros dos ejercicios de laboratorio 2 unidad 2

---
+++ carpeta_tarea/lab2 numeroPrimeroSegundo.cpp
@@ -0,0 +1,43 @@
+/*Escribir un programa que lea dos números, nombrados como primero y
+segundo, y luego indique como salida cuál de los dos (primero o segundo) es el
+mayor de los dos números ingresados.*/
+
+//PARTE 1: LIBRERIAS
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+
+//PARTE 2: DATOS/VARIABLES
+int numeroPrimero = 0;
+int numeroSegundo = 0;
+char continuar = 'n';
+
+//PARTE 3: FUNCION PRINCIPAL
+
+int main()
+	//PARTE 4: INICIO DEL CODIDO
+{
+	do{
+		//PARTE 5: INSTRUCCIONES O CODIGO
+		printf("\nEste programa calcula el mayor de dos numeros \n");
+		printf("Ingrese el primer numero a evaluar: \n");
+		scanf("%d",&numeroPrimero);
+		printf("Ingrese el segundo numero a evaluar: \n");
+		scanf("%d", &numeroSegundo);
+		
+		//hago mi comparacion
+		if(numeroPrimero > numeroSegundo ){
+			printf("El primer numero es %d mayor que %d \n", numeroPrimero,numeroSegundo);
+			return 0;
+		} else if(numeroSegundo > numeroPrimero){
+			printf("El segundo numero es %d mayor que %d \n",numeroSegundo,numeroPrimero);
+		}
+		
+		//si desea volver a comparar numeros o se sale
+		printf("\nDesea continuar con otro numero s/n");
+		continuar=getch();//Para asignar a una variable char
+		printf("\n");
+		
+	}while(continuar == 's');//si continua dentro del ciclo
+	//PARTE 6:FIN DEL CODIGO 
+}
---
+++ carpeta_tarea/lab2 tres numeros mayor-menor.cpp
@@ -0,0 +1,61 @@
+/*Escribir un programa que permita leer tres números y luego indique cuál es el
+mayor, y cuál es el menor de los tres.*/
+
+//PARTE 1: LIBRERIAS
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+
+//PARTE 2: DATOS/VARIABLES
+int primerNumero = 0;
+int segundoNumero = 0;
+int tercerNumero = 0;
+char continuar = 'n';
+
+//PARTE 3: FUNCION PRINCIPAL
+
+int main()
+	//PARTE 4: INICIO DEL CODIDO
+{
+	do{
+		//PARTE 5: INSTRUCCIONES O CODIGO
+		printf("\nEste programa calcula el mayor de tres numeros \n");
+		printf("Ingrese el primer numero a evaluar: \n");
+		scanf("%d",&primerNumero);
+		printf("Ingrese el segundo numero a evaluar: \n");
+		scanf("%d", &segundoNumero);
+		printf("Ingrese el tercer numero a evaluar: \n");
+		scanf("%d", &tercerNumero);
+		
+		//Hago comparacion para hallar el numero mayor
+		if((primerNumero > segundoNumero)&&(primerNumero > tercerNumero)){
+			printf("\nEl número mayor de los tres es: \n");
+			printf("El primer numero es %d  \t mayor que %d y %d \n", primerNumero, segundoNumero, tercerNumero);
+		} else if((segundoNumero > tercerNumero)&&(segundoNumero>primerNumero)){
+			printf("\nEl número mayor de los tres es: \n");
+			printf("El segundo numero es %d  \t mayor que %d y %d \n", segundoNumero, primerNumero, tercerNumero);
+		} else{
+			printf("\nEl número mayor de los tres es: \n");
+			printf("El tercer numero es %d  \t mayor que %d y %d \n",tercerNumero, primerNumero,segundoNumero);
+		}
+		
+		//hago mi comparacion para hallar el numero menor
+		if((primerNumero < segundoNumero)&&(primerNumero < tercerNumero)){
+			printf("\nEl número menor de los tres es: \n");
+			printf("El primer numero es %d  \t menor que %d y %d \n", primerNumero, segundoNumero, tercerNumero);
+		} else if((segundoNumero < tercerNumero)&&(segundoNumero < primerNumero)){
+			printf("\nEl número menor de los tres es: \n");
+			printf("El segundo numero es %d  \t menor que %d y %d \n", segundoNumero, primerNumero, tercerNumero);
+		} else{
+			printf("\nEl número menor de los tres es: \n");
+			printf("El tercer numero es %d  \t menor que %d y %d \n",tercerNumero, primerNumero,segundoNumero);
+		}
+		
+		//si desea volver a comparar numeros o se sale
+		printf("\nDesea continuar con otro numero s/n");
+		continuar=getch();//Para asignar a una variable char
+		printf("\n");
+		
+	}while(continuar == 's');//si continua dentro del ciclo
+	//PARTE 6:FIN DEL CODIGO 
+}
