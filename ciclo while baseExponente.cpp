7df1798e22aebdda138132e87b35d0f61b152b66
juanySC <48737486+juanySC@users.noreply.github.com>
2025-04-08 12:26:33 -0600

    U2 ejercicio 3 ciclos

---
+++ ciclo while baseExponente.cpp
@@ -0,0 +1,46 @@
+/*Ejercicio 5: Leer los números A y B, mostrar como salida el resultado de 
+elevar el número base A al exponente B
+A = 5	B=3		5*5*5= 125	*/
+
+//PARTE 1: LIBRERIAS
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+
+//PARTE 2: DATOS/VARIABLES
+int base = 0;
+int exponente = 0;
+char continuar = 'n';
+
+//PARTE 3: FUNCION PRINCIPAL
+int main()
+	//PARTE 4: INICIO DEL CODIDO
+{
+	do{	//PARTE 5: INSTRUCCIONES O CODIGO
+		
+		//solicitando datos
+		printf("\nIngrese la base\n");
+		scanf("%d",&base);
+		printf("\nIngrese el exponente\n");
+		scanf("%d",&exponente);
+		
+		//ciclo descendente
+		int contador = 1;
+		int resultado = 1;
+		
+		while(contador <= exponente){
+			resultado = resultado*base;
+			contador++;
+		}
+		
+		printf("\n El resultado es: %d",resultado);
+		
+		
+		//si desea volver a comparar numeros o se sale
+		printf("\n\nDesea continuar con otro numero s/n");
+		continuar=getch();//Para asignar a una variable char
+		printf("\n");
+	}while(continuar = 's');
+	
+	//PARTE 6:FIN DEL CODIGO 
+}
