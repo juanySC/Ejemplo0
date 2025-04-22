e197ee395355a73869c1655a2ea472a099e83bd8
juanySC <48737486+juanySC@users.noreply.github.com>
2025-04-22 11:26:54 -0600

    Add files via upload

---
+++ Ejemplo gotoxy.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <windows.h>
+#include <conio.h>
+
+//proptotipo
+int gotoxy(int x, int y);
+
+int main(){
+	
+	gotoxy(1,1); printf("Esquina superior izquierda");
+	gotoxy(1,18); printf("Esquina superior derecha");
+	gotoxy(70,1); printf("Superior");
+	gotoxy(70,18); printf("Inferior");
+	
+	getch();
+	
+}
+
+//desarrollo de la funcion
+int gotoxy(int x, int y){
+	COORD coord;
+	HANDLE h_stdout;
+	
+	coord.X = x;
+	coord.Y = y;
+	
+	if((h_stdout = GetStdHandle(STD_OUTPUT_HANDLE))== INVALID_HANDLE_VALUE)
+		return 0;
+	if(SetConsoleCursorPosition(h_stdout, coord) == 0)
+		return 0;
+	return 1;
+	
+}
