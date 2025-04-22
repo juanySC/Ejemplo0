9624b5e2b0f8430dd82a5a5f88cdf7cd8679d71a
juanySC <48737486+juanySC@users.noreply.github.com>
2025-04-22 12:03:16 -0600

    Add files via upload
    sale de manera horizontal y no vertical

---
+++ avanzadoC/Margen gotoxy.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <windows.h>
+#include <conio.h>
+
+//proptotipo
+int gotoxy(int x, int y);
+
+int contadorX;
+int contadorY;
+
+int main(){
+
+	//sirve para que se repita de  1 a n
+	contadorX = 1;
+	contadorY = 1;
+
+		//derecha-abajo
+		while(contadorX <= 50){
+			gotoxy(contadorX, 50); printf("x");
+			contadorX++;
+		}
+		
+		while(contadorY <=50){
+			gotoxy(50, contadorY); printf("x");
+			contadorY++;
+		}
+		
+		//reinicio el contador
+		contadorX = 1;
+		contadorY = 1;
+		//izquierda arriba
+		while(contadorX <= 50){
+			gotoxy(contadorX, 1); printf("x");
+			contadorX++;
+		}
+		
+		while(contadorY <= 50){
+			gotoxy(1, contadorY), printf("x");
+			contadorY++;
+		}
+	
+	getch();
+	
+}
+	
+	//desarrollo de la funcion
+	int gotoxy(int x, int y){
+		COORD coord;
+		HANDLE h_stdout;
+		
+		coord.X = x;
+		coord.Y = y;
+		
+		if((h_stdout = GetStdHandle(STD_OUTPUT_HANDLE))== INVALID_HANDLE_VALUE)
+			return 0;
+		if(SetConsoleCursorPosition(h_stdout, coord) == 0)
+			return 0;
+		return 1;
+		
+	}
