9d023396d2e30789776e94b59be46d3bb350f058
juanySC <48737486+juanySC@users.noreply.github.com>
2025-04-22 12:52:03 -0600

    no esta temrinado

---
+++ avanzadoC/resolucion gotoxy.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <windows.h>
+#include <conio.h>
+
+//proptotipo
+int gotoxy(int x, int y);
+
+int contadorX;
+int contadorY;
+
+int main(){
+	
+	//sirve para que se repita de  1 a n
+	contadorX = 1;
+	contadorY = 1;
+	
+	gotoxy(1,18); printf("Esquina inferior izquierda");
+	gotoxy(70,1); printf("Superior");
+	gotoxy(70,1); printf("Superior");
+	gotoxy(70,18); printf("Inferior");
+	
+	int px = 1, int py =2;
+	//derecha-abajo
+	while(px <= 70){
+		
+		gotoxy(px, py); printf("x");
+		Sleep(100);
+		contadorX++;
+		
+		if(py<= 2) py++;
+		if(py>=17) py--;
+	}
+
+	
+	getch();
+	
+}
+	
+	//desarrollo de la funcion
+	int gotoxy(int x, int y){
+		COORD coord;
+		HANDLE h_stdout;
+		
+		coord.X = x;
+		coord.Y = y;
+		
+		if((h_stdout = GetStdHandle(STD_OUTPUT_HANDLE))== INVALID_HANDLE_VALUE)
+			return 0;
+		if(SetConsoleCursorPosition(h_stdout, coord) == 0)
+			return 0;
+		return 1;
+		
+	}
+		
