35c8094f04416c2dc5c7f5388d86b1bddadca5f6
juanySC <48737486+juanySC@users.noreply.github.com>
2025-04-30 08:48:45 -0600

    Add files via upload

---
+++ avanzadoC/resolucion ejercicio 2 arreglos.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <windows.h>
+#include <time.h>
+int vector[10];
+
+int main (){
+	//para ver los numero mayor y menor
+	int max = vector[0], indice_max = 0;
+	int min = 100000, indice_min = 0;
+	
+	printf("Ingrese los valores\t");
+	srand(unsigned(time(NULL)));
+	for(int i = 0; i < 10; i++){
+		printf("\t %1s %d %1s ","{", i, "}" );
+		vector[i] = rand()%101;
+		printf("%10d\n", vector[i]);
+		if(max<vector[i]){
+			max = vector[i];
+			indice_max = i;
+		}
+		if(min>vector[i]){
+			min = vector[i];
+			indice_min = i;
+		}
+		Sleep(300);
+	}
+	
+	printf("\nValor maximo \t%d", max);
+	printf("\tubicado en maximo \t%d", indice_max);
+	printf("\nValor minimo\t%d", min);
+	printf("\tubicado en minimo \t%d", indice_min);
+}
