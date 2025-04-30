1f009cff4f2027a914a37d631468bfe677d0dd59
juanySC <48737486+juanySC@users.noreply.github.com>
2025-04-30 08:41:41 -0600

    Add files via upload

---
+++ avanzadoC/resolucion ejerciccio 1 arreglos.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int vector[10];
+
+int main (){
+	//para ver los numero mayor y menor
+	int max = vector[0], indice_max = 0;
+	int min = 100000, indice_min = 0;
+	
+	printf("Ingrese los valores\t");
+	for(int i = 0; i < 10; i++){
+		printf("\t %1s %d %1s ","{", i, "}" );
+		scanf("%d", &vector[i]);
+		if(max<vector[i]){
+			max = vector[i];
+			indice_max = i;
+		}
+		if(min>vector[i]){
+			min = vector[i];
+			indice_min = i;
+		}
+	}
+	
+	printf("\nValor maximo \t%d", max);
+	printf("\tubicado en maximo \t%d", indice_max);
+	printf("\nValor minimo\t%d", min);
+	printf("\tubicado en minimo \t%d", indice_min);
+}
