1ddce21233d0970f787113a7e85cfefb0409290e
juanySC <48737486+juanySC@users.noreply.github.com>
2025-05-01 18:47:44 -0600

    numeros 1-100 elevado al cuadrado

---
+++ avanzadoC/arreglos tarea1 ejercicio1 cuadrado de los numers.cpp
@@ -0,0 +1,24 @@
+/*Un programa que tenga 100 espacios y en los mismos ubicar el cuadrado de los
+ cien primeros números  enteros  y  a  continuación  escribir  la sumatoria del 
+contenido del vector*/
+#include <stdio.h>
+#define NUM 100
+
+int main() {
+	int cuadrados[NUM];
+	int suma = 0;
+	
+	printf("Este programa muestra el cuadrado del 1 al 100");
+	for (int i = 0; i < NUM; i++) {
+		//para que no comienze de 0 le sumo +1
+		cuadrados[i] = (i + 1) * (i + 1);
+		printf("\n %1s %2d %3s \t","{",i,"}");
+		printf("%5d ", cuadrados[i]);
+		suma = suma + cuadrados[i];
+	}
+	
+	//muestro la sumatoria
+	printf("\n \nLa suma del 1 al 100 es: %d", suma);
+	
+	return 0;
+}
