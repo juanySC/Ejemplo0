a1fa035ef47fd6acd4628de7dc61eb5df3c3a806
juanySC <48737486+juanySC@users.noreply.github.com>
2025-05-07 06:01:11 -0600

    resuelto por funciones

---
+++ avanzadoC/matriz problema2 3x3 contradiagonal.cpp
@@ -0,0 +1,69 @@
+/*Problema 2:  Escribir un programa que gestione una matriz de números enteros 
+con 3 filas y 3 columnas, llenar con valores aleatorios de 21 a 30, generar como
+ salida la suma de los valores contenidos en la contradiagonal de la matriz.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <time.h>
+#include <windows.h>
+#define FILA 3
+#define COLUMNA 3
+
+
+int matrix[FILA][COLUMNA];
+int filas = 0, columnas = 0;
+char continuar = 'n';
+
+void valoresAleatorios(){
+	//asignar valores aleatorios
+	srand(unsigned(time(NULL)));
+	//reiniciar las variables
+	//valores aleatorios
+	for(filas = 0; filas<FILA; filas++){
+		for(columnas = 0; columnas<COLUMNA; columnas++){
+			//genere de 21 a 30, por eso sumo 21 para no empezar en 0
+			matrix[filas][columnas] = 21 + rand()%10 ;
+		}
+	}
+}
+
+void impresionDatos(){
+	printf("\nContenido actual: \n");
+	//imprimo el contenido
+	for(filas = 0; filas<FILA; filas++){
+		for(columnas = 0; columnas<COLUMNA; columnas++){
+			//imprimo los valores de la matriz
+			printf("%5d", matrix[filas][columnas]);
+			
+		}
+		printf("\n");
+	}
+}	
+
+int sumaContradiagonal(){
+	int suma = 0;
+	/*matrix[0][0];
+	matrix[1][1];
+	matrix[2][0];	*/ 
+	for(filas = 0; filas<3; filas++){ 
+		suma += matrix[filas][2-filas];
+	}
+	return(suma);
+}	
+
+
+int main() {
+	do{
+		valoresAleatorios();
+		impresionDatos();
+		printf("\nLa suma de la contradiagonal es = %5d", sumaContradiagonal());
+		
+		//si desea volver a meter ecuacion cuadratica
+		printf("\n\nDesea imprimir de nuevo s/n");
+		continuar=getch();//Para asignar a una variable char
+		printf("\n");
+	}while(continuar == 's');
+	return 0;
+}
+
---
+++ avanzadoC/matriz problema2 4x5 aleatorio.cpp
@@ -0,0 +1,75 @@
+/*Escribir un programa que gestione una matriz de números enteros con 4 filas y
+ 5 columnas, llenar con valores aleatorios 1 a 100, generar como salida la suma 
+todos los valores contenidos en la matriz. Mostrar los datos contenidos en la
+ matriz en formato tabla. [Filas] [Columnas].*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <time.h>
+#define COLUMNA 5
+#define FILA 4
+
+int matrix[FILA][COLUMNA];
+int filas = 0;
+int columnas = 0;
+
+char continuar = 'n';
+
+void valorAleatorio(){
+	//asignar valores aleatorios
+	srand(unsigned(time(NULL)));
+	//reiniciar las variables
+	//valores aleatorios
+	for(filas = 0; filas<FILA; filas++){
+		for(columnas = 0; columnas<COLUMNA; columnas++){
+			//genere numeros de 1 a 100, por eso sumo +1
+			matrix[filas][columnas] = 1 + rand()%100 ;
+		}
+	}
+}
+	
+void impresionDatos(){
+	printf("\nContenido actual: \n");
+	//imprimo el contenido
+	for(filas = 0; filas<FILA; filas++){
+		for(columnas = 0; columnas<COLUMNA; columnas++){
+			//genere de 25 a 30, son 5 valores
+			printf("%5d", matrix[filas][columnas]);
+		}
+		printf("\n");
+	}
+	
+}	
+	
+int sumaMatrix(){
+	//lo declaro aqui para que no acumule la suma anterior y se reinicie cada
+	//vez que realize la operacion
+	int suma = 0;
+	
+	for(filas = 0; filas<FILA; filas++){
+		for(columnas = 0; columnas<COLUMNA; columnas++){
+			//la suma solo la genero una vez porque sino sumaria dos veces si 
+			//lo añado al ciclo de filas
+			suma += matrix[filas][columnas];
+		}
+	}
+	//tengo que retornar un valor de la suma
+	return(suma);
+}
+
+
+int main() {
+	do{
+		//llamando funciones
+		valorAleatorio();
+		impresionDatos();
+		printf("La suma de total de todas la matriz es: %5d", sumaMatrix());
+		
+		//si desea volver a meter ecuacion cuadratica
+		printf("\n\nDesea imprimir de nuevo s/n");
+		continuar=getch();//Para asignar a una variable char
+		printf("\n");
+	}while(continuar == 's');
+	return 0;
+}
