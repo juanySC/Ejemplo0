91de1eeb25fd26ebb251818c2dffc0ca98798117
juanySC <48737486+juanySC@users.noreply.github.com>
2025-05-18 21:36:39 -0600

    correcion de main

--- libro/ejercicio1 libro1 punteros.cpp
+++ libro/ejercicio1 libro1 punteros.cpp
@@ -2,7 +2,7 @@
 #include <math.h> 
 void potrat1(double x, double y);
 void potrat2(double* x, double* y); 
-void main() 
+int main() 
 { 
 	double a, b; 
 	a = 5.0; b = 1.0e2; 
@@ -10,6 +10,7 @@ void main()
 	printf("\n a = %.1f b = %.1lf",a,b); 
 	potrat2(&a, &b); 
 	printf("\n a = %.1f b = %.1lf",a,b);
+	return 0;
 }
 void potrat1(double x, double y) 
 { 
