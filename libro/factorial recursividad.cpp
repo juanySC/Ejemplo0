33aa001b0cd20d9969222694f1c09bcc1e8c337e
juanySC <48737486+juanySC@users.noreply.github.com>
2025-05-19 18:11:55 -0600

    resuelto con recursividad

---
+++ libro/factorial recursividad.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int factorialEntero(int numero);
+float factorialFraccion(float numeroF);
+
+int main(){
+	int numero;
+	//factorial por numeros enteros
+	printf("Ingrese el numero que desee sacar el factorial");
+	scanf("%d", &numero);
+	printf("El resultado es: %d", factorialEntero(numero));
+	//factorial por numeros fraccionarios
+	printf("El resultado es:; %.5f", factorialFraccion(numero));
+	return 0;
+}
+
+	//n!
+int factorialEntero(int numero){
+	//cuando sea el resultado cero lo tomara como 1 y ya devuelve todo el resultado
+	if( numero == 0) return 1; 
+	//ej. 9 * 9- 1 = 9 * 8
+	return numero * factorialEntero(numero-1);
+}	
+	
+//1/n!	
+float factorialFraccion(float numeroF){
+	
+	float factorial = 1.0;
+	for (int i = 1; i <= (int)numeroF; i++) {
+		factorial *= i;
+	}
+	return 1.0 / factorial;
+	
+	/*int entero = (int)numeroF; // Convierte a entero (solo trabaja para valores enteros positivos)
+	if (entero == 0) return 1.0;
+	return 1.0 / factorialEntero(entero);*/
+}	
+	
---
+++ libro/fibonacci recursividad.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int fibonacci(int numero);
+int sumaNaturales(int natural);
+
+int main(){
+	int numero;
+	printf("El programa de fibonacci y los numero naturales");
+	printf("Ingrese el número que desee: ");
+	scanf("%d", &numero);
+	printf("La suma de fibonacci es:  %d", fibonacci(numero));
+	//para la suma de los naturales 
+	printf("la suma de los numeros naturales: %d", sumaNaturales(numero));
+	return 0;
+}
+
+int fibonacci(int numero){
+	if(numero ==0) return 0;
+	if(numero ==1) return 1;
+	return fibonacci(numero-1) + (numero -2);
+}
+
+int sumaNaturales(int natural){
+	if(natural == 0) return 0;
+	return natural + sumaNaturales(natural -1);
+}	
+	
+
